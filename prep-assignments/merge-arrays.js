ea41bf83da00d331e34916f536caccfd2df6ab52
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-03-23 14:17:22 -0600

    merge arrays

---
+++ prep-assignments/merge-arrays.js
@@ -0,0 +1,10 @@
+const merge = function(arrayOne, arrayTwo) {
+  let concatenate = arrayOne.concat(arrayTwo);
+  let sorted = concatenate.sort();
+  return sorted;
+}
+
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
